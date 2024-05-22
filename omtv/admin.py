74450ebf96c21ac45bad0fdc06017cd115271eb0
slissill <slissill@gmail.com>
2024-05-22 17:47:27 +0200

    réglages

--- omtv/admin.py
+++ omtv/admin.py
@@ -5,21 +5,20 @@ class ChannelAdmin(admin.ModelAdmin):
     list_display = ('name', 'sort')
 
 class ProgrammeAdmin(admin.ModelAdmin):
-    # config en mode liste
+    
+    #********  config en mode liste ********************
     list_filter = ('genre', 'channel', )
     search_fields = ['title', 'genre']
-    list_per_page = 8
-
-    # config en mode fiche    
+    list_per_page = 50
+    readonly_fields = ['created_at', 'updated_at']
+    
+    #******** config en mode fiche *********************
     #fields = ['title', 'genre', 'pdate', 'start', 'stop']              # <= on ne peut pas faire les 2 en meme temps
     fieldsets = [
         ('Timing', {'fields' : ['pdate', 'start', 'stop']}), 
         ('Autres', {'fields' : ['title', 'genre']}), 
+        ('Audit', {'fields' : ['created_at', 'updated_at']}), 
     ]
-
-
-
-
 
 
 # Register your models here.
--- omtv/urls.py
+++ omtv/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "omtv"
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.programmes, name='home'),
     path('home', views.home, name='home'),
     path('programmes', views.programmes, name='programmes'),    
     path('preferences', views.preferences, name='preferences'),
